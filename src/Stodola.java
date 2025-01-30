4827e3d59d2bc125b935713fcaa71a1ca9ffac85
jonas <jonassekyra@gmail.com>
2025-01-30 10:40:08 +0100

    pridani metody na koupi maleho zvirete, metoda kontroluje zda je na zvire misto.

--- src/Stodola.java
+++ src/Stodola.java
@@ -3,4 +3,10 @@ import java.util.ArrayList;
 public class Stodola {
     private ArrayList<Animal> malaZvirata;
     private ArrayList<Animal> velkaZvirata;
+
+    public void koupitMaleZvire(Animal animal) {
+        if (malaZvirata.size() <= 10) {
+            malaZvirata.add(animal);
+        }
+    }
 }
