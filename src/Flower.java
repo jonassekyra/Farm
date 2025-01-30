491c0399847e76c19c997715084fcde584844a18
jonas <jonassekyra@gmail.com>
2025-01-30 10:28:24 +0100

    metody get a set + metoda na vypocet sance na rust

--- src/Flower.java
+++ src/Flower.java
@@ -17,4 +17,49 @@ public abstract class Flower {
                 '}';
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getNeededArea() {
+        return neededArea;
+    }
+
+    public void setNeededArea(double neededArea) {
+        this.neededArea = neededArea;
+    }
+
+    public double getChanceOfGrowth() {
+        return chanceOfGrowth;
+    }
+
+    public void setChanceOfGrowth(double chanceOfGrowth) {
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public boolean isWatered() {
+        return isWatered;
+    }
+
+    public void setWatered(boolean watered) {
+        isWatered = watered;
+    }
+
+    public void sanceNaRust(){
+        if(isWatered()){
+            chanceOfGrowth = chanceOfGrowth + 40;
+        }
+    }
 }
