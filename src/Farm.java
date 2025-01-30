29d632ba5e8afcd8e3b9bb18ff0f4d9be2430279
jonas <jonassekyra@gmail.com>
2025-01-30 10:24:37 +0100

    pridani metod na zasazeni kytek, zavlazeni a zklieni

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 public class Farm {
+
+    private ArrayList<Flower> kytky;
+
+    public void pridaniRostlin(Flower f){
+        kytky.add(f);
+    }
+
+    public void zavlazeniRostlin(Flower f){
+        f.setWatered(true);
+    }
+
+    public void zklizeniRostlin(Flower f){
+        kytky.remove(f);
+    }
+
+
 
 
 
